9a3d17bb0aaf59d6aea0755003a0536d6b35a8e9
adhithyapp <51515095+adhithyapp@users.noreply.github.com>
2019-12-13 11:49:36 +0530

    Add files via upload

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="fileupload2mvc.MvcApplication" Language="C#" %>
---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(fileupload2mvc.Startup))]
+namespace fileupload2mvc
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
