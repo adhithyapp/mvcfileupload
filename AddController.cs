00234164d529ae2618f43cb5a0287d9a52c4c9b2
adhithyapp <51515095+adhithyapp@users.noreply.github.com>
2019-12-13 11:58:36 +0530

    Add files via upload

---
+++ AddController.cs
@@ -0,0 +1,36 @@
+﻿using fileupload2mvc.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace fileupload2mvc.Controllers
+{
+    public class AddController : Controller
+    {
+
+        DemoMVCEntities db = new DemoMVCEntities();
+        // GET: Add
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Register()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Register(tbl_add_user tu , HttpPostedFileBase file)
+        {
+            string myfile = file.FileName;
+            tu.profile = myfile;
+            var path = Path.Combine(Server.MapPath("~/upload/"), myfile);
+            file.SaveAs(path);
+            db.tbl_add_user.Add(tu);
+            db.SaveChanges();
+            return View();
+        }
+    }
+}
